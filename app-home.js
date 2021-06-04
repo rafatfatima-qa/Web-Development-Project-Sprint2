82445af3f43f79504cbdb94289e63cddb0792351
unknown <fatimaxaidi@yahoo.com>
2021-06-05 02:59:17 +0500

    Rafat Fatima's Portfolio

--- app-home.js
+++ app-home.js
@@ -6,7 +6,7 @@ function scrollFunction() {
     document.getElementById("navbar").style.padding = "10px 10px";
     document.getElementById("logo").style.fontSize = "25px";
   } else {
-    document.getElementById("navbar").style.padding = "50px 10px";
+    document.getElementById("navbar").style.padding = "30px 10px";
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
