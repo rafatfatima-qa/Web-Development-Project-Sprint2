4f27928896f36a8c1bd022aba57c12280021e585
unknown <fatimaxaidi@yahoo.com>
2021-06-18 23:15:26 +0500

    This is Rafat Fatima's Portfolio

--- app-home.js
+++ app-home.js
@@ -2,11 +2,24 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "10px 10px";
-    document.getElementById("logo").style.fontSize = "25px";
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 10) {
+    document.getElementById("navbar").style.padding = "20px 10px";
+    document.getElementById("logo").style.fontSize = "15px";
   } else {
-    document.getElementById("navbar").style.padding = "30px 10px";
-    document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("navbar").style.padding = "70px 10px";
+    document.getElementById("logo").style.fontSize = "25px";
   }
 }
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding = "5px 5px";
+//     document.getElementById("logo").style.fontSize = "10px";
+//   } else {
+//     document.getElementById("navbar").style.padding = "20px 20px";
+//     document.getElementById("logo").style.fontSize = "35px";
+//   }
+// }
